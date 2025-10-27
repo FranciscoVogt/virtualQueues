b648d7f186cf7b981eed0283480e9009d2541bd2
FranciscoVogt <franciscogermanovogt@gmail.com>
2025-10-27 14:11:06 +0100

    up

--- controlPlane.py
+++ controlPlane.py
@@ -6,7 +6,7 @@ fwd_table = p4.SwitchIngress.forward
 vq_table = p4.SwitchIngress.getQueueLimit
 
 
-fwd_table.add_with_send(dst_addr = IPAddress('192.168.0.20'), port = 160, id = 5)
+fwd_table.add_with_send(dst_addr = IPAddress('192.168.0.20'), port = 164, id = 5)
 
 vq_table.add_with_qLimit(qID = 5, qLimit = 10)
 
